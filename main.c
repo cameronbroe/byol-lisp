9515aed67dbb91b9a7408700e6a702c7b939b836
Cameron Roe <cameron.b.roe@gmail.com>
2019-11-05 21:13:36 -0600

    Simple interactive prompt

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+#define PROGRAM_NAME "byol-lisp"
+#define VERSION "0.0.1.dev"
+
+// Input buffer for REPL
+static char input[2048]; // 2 KB buffer
+
+void __print_version();
 
 int main(int argc, char** argv) {
-    printf("Hello, world!");
+    __print_version();
+    while(1) {
+        printf("%s> ", PROGRAM_NAME);
+        fgets(input, 2048, stdin);
+
+        if(strstr(input, "!quit") != NULL) {
+            break;
+        }
+        printf("The hell is a %s", input);
+    }
     return 0;
+}
+
+void __print_version() {
+    printf("%s ver. %s\n", PROGRAM_NAME, VERSION);
+    printf("Ctrl+C or !quit to Exit\n");
 }
