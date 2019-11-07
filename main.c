ff4b595dcee3f4be64d203d2ea0385de91ac8fef
Cameron Roe <cameron.b.roe@gmail.com>
2019-11-06 22:30:54 -0600

    Chapter 7 is complete! Working polish notation calculator

--- main.c
+++ main.c
@@ -19,6 +19,8 @@ static char input[2048]; // 2 KB buffer
 
 void __print_version();
 long evaluate_program(mpc_ast_t* node);
+long evaluate_operator(char* operator, long x, long y);
+void evaluate_command(char* cmd);
 
 int main(int argc, char** argv) {
     __print_version();
@@ -53,7 +55,7 @@ int main(int argc, char** argv) {
         printf("%s> ", PROGRAM_NAME);
         fgets(input, 2048, stdin);
         int len = strlen(input);
-        *(input + (len - 2)) = '\0';
+        input[strcspn(input, "\r\n")] = '\0';
 #else
         char prompt[66];
         sprintf(prompt, "%s> ", PROGRAM_NAME);
@@ -65,8 +67,8 @@ int main(int argc, char** argv) {
         // Parse the input
         mpc_result_t result;
         if(mpc_parse("<stdin>", input, prog, &result)) {
-            long number_of_nodes = evaluate_program(result.output);
-            printf("Your expression has %ld nodes\n", number_of_nodes);
+            long output = evaluate_program(result.output);
+            printf("%s = %ld\n", input, output);
             mpc_ast_delete(result.output);
         } else {
             mpc_err_print(result.error);
@@ -87,16 +89,49 @@ void __print_version() {
 }
 
 long evaluate_program(mpc_ast_t* node) {
-    printf("tag: %s\ncontents: %s\n\n", node->tag, node->contents);
-    if(node->children_num == 0) {
-        return 1;
-    } else if(node->children_num > 0) {
-        long total = 1;
-        for(int i = 0; i < node->children_num; i++) {
-            total += evaluate_program(node->children[i]);
-        }
-        return total;
+    // Implement commands
+    if(strstr(node->tag, "cmd") != NULL) {
+        evaluate_command(node->contents);
+        return 0;
+    }
+
+    // Numbers should just be converted
+    if(strstr(node->tag, "number") != NULL) {
+        return atol(node->contents);
+    }
+
+    // Check operator in second child
+    char* operator = node->children[1]->contents;
+
+    // Next child is an expression, evaluate it and store it in a variable
+    long x = evaluate_program(node->children[2]);
+
+    // Iterate the rest of the expressions
+    for(int i = 3; strstr(node->children[i]->tag, "expr") != NULL; i++) {
+        x = evaluate_operator(operator, x, evaluate_program(node->children[i]));
+    }
+
+    return x;
+}
+
+long evaluate_operator(char* operator, long x, long y) {
+    if(strcmp(operator, "+") == 0) {
+        return x + y;
+    } else if(strcmp(operator, "-") == 0) {
+        return x - y;
+    } else if(strcmp(operator, "*") == 0) {
+        return x * y;
+    } else if(strcmp(operator, "/") == 0) {
+        return x / y;
+    } else if(strcmp(operator, "%") == 0) {
+        return x % y;
     } else {
         return 0;
+    }
+}
+
+void evaluate_command(char* cmd) {
+    if(strcmp(cmd, "quit") == 0) {
+        exit(0);
     }
 }
